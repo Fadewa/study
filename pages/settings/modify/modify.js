d4d588c962115a1c01d21b5f86ccc98a904bcc42
Jordan <654941292@qq.com>
2017-12-29 17:01:25 +0800

    添加设置功能

---
+++ pages/settings/modify/modify.js
@@ -0,0 +1,87 @@
+// pages/settings/modify/modify.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    mobile:'',
+    settings:false
+  },
+  //监听验证码输入框
+  inputCode:function(){
+
+  },
+
+  //获取二维码
+  getcode:function(){
+
+  },
+  //监听新密码输入
+  inputPsw:function(){
+
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that=this
+    wx.getStorage({
+      key: 'password',
+      success: function(res) {
+        that.setData({
+          mobile:res.data.mobile
+        })
+      },
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
---
+++ pages/settings/settings.js
@@ -0,0 +1,108 @@
+// pages/settings/settings.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    show:false,
+    storage:''
+  },
+  //修改密码
+  modify:function(){
+    wx.navigateTo({
+      url: '/pages/settings/modify/modify',
+    })
+  },
+  //清除缓存
+  clearStorage:function(){
+    wx.showLoading({
+      title: '清理中...',
+    })
+    wx.clearStorage({
+      success:function(){
+        wx.hideLoading()
+        wx.showToast({
+          title: '清理成功',
+        })
+      }
+    })
+  },
+  //关于我们
+  about:function(){
+    wx.navigateTo({
+      url: '',
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this
+    // 获取缓存大小
+    wx.getStorageInfo({
+      success: function (res) {
+        that.setData({
+          storage: res.currentSize
+        })
+      },
+    })
+    wx.getStorage({
+      key: 'password',
+      success: function (res) {
+        that.setData({
+          show: true
+        })
+      },
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
